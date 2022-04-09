df550b8cbf73f8b767a35f945baa5c73959733a1
lkarow <lkarow@online.de>
2022-04-09 12:03:40 +0200

    create repository which contains functions and pokemonList in IIFE
    wrap pokemonList in IIFE inside pokemonRepository
    
    add functions to access pokemonRepository
    
    refactor to use forEach() instead of the for loop to iterate over the repository array

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,66 @@
-let pokemonList = [];
-pokemonList = [
-  {
-    name: 'Bulbasaur', 
-    height: 7,
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Charmander', 
-    height: 6,
-    types: ['fire']
-  },
-  {
-    name: 'Squirtle', 
-    height: 5,
-    types: ['water']
-  }
-];
+let pokemonRepository = (function() {
+  // pokemonList Array
+  let pokemonList = [];
+  pokemonList = [
+    {
+      name: 'Bulbasaur', 
+      height: 7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Charmander', 
+      height: 6,
+      types: ['fire']
+    },
+    {
+      name: 'Squirtle', 
+      height: 5,
+      types: ['water']
+    }
+  ];
 
-// iterate pokemonList with forEach
-pokemonList.forEach(function(pokemon) {
+  // function to return all items in pokemonList array
+  function getAll () {    
+    return pokemonList;
+  };
+
+  // function to add a single item to the pokemonList array
+  function add (pokemon) {
+    // check typeof for object and if all keys exist
+    if (
+      typeof pokemon === 'object' && 
+      'name' in pokemon &&
+      'height' in pokemon &&
+      'types' in pokemon) {
+        pokemonList.push(pokemon)
+    } else {
+        console.log('Error!')
+      }
+  };
+
+  // function to find pokemon by name and return it as a new array
+  function find (pokemonName) {
+    return pokemonList.filter(function (pokemon) {
+      return pokemon.name === pokemonName
+    });
+  };
+
+  return {
+    getAll,
+    add,
+    find
+  };
+
+})();
+
+// // iterate pokemonList with forEach
+pokemonRepository.getAll().forEach(function(pokemon) {
   // check height and add comments
   let comment = '';
   if (pokemon.height <= 5) {
     comment = ' - It\'s so tiny!'
   } else if (pokemon.height >= 7) { 
-      comment = ' - Wow, that’s big!';
+      comment = ' - Wow, that\'s big!';
   }
   document.write(`<span class="pokename">${pokemon.name}</span> (height: ${pokemon.height}, types: ${pokemon.types.join(', ')})${comment}<br>`);
 });
